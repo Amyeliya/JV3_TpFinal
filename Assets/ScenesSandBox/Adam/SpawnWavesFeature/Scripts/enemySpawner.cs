c3c90175be6432137ff4dde932e20c4caa9ce205
adamelalfy <143737572+adamelalfy@users.noreply.github.com>
2024-11-22 15:17:09 -0500

    spawning de base implémenté

--- Assets/ScenesSandBox/Adam/SpawnWavesFeature/Scripts/enemySpawner.cs
+++ Assets/ScenesSandBox/Adam/SpawnWavesFeature/Scripts/enemySpawner.cs
@@ -6,5 +6,29 @@ public class enemySpawner : MonoBehaviour
 {
 
     [SerializeField] private GameObject enemy;
+    [SerializeField] private GameObject spawnZone;
+    private float _interval = 0.5f;
+
+    private void spawnEnemy()
+    {
+        Transform spawnZoneTransform = spawnZone.transform;
+        Vector3 spawnZoneScale = spawnZoneTransform.localScale;
+        Vector3 spawnZonePosition = spawnZoneTransform.position;
+
+        Vector3 randomPosition = new Vector3(
+            Random.Range(spawnZonePosition.x - spawnZoneScale.x / 2, spawnZonePosition.x + spawnZoneScale.x / 2),
+            Random.Range(spawnZonePosition.y - spawnZoneScale.y / 2, spawnZonePosition.y + spawnZoneScale.y / 2),
+            Random.Range(spawnZonePosition.z - spawnZoneScale.z / 2, spawnZonePosition.z + spawnZoneScale.z / 2)
+        );
+        Instantiate(enemy, randomPosition, Quaternion.identity);
+    }
+
+    private void Start() {
+        if(1 == 1)
+        {
+        InvokeRepeating("spawnEnemy", 0f, _interval);
+
+        }
+    }
 
 }
