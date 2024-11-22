845f956cc31b7b6983c56d33f1783ea197307237
adamelalfy <143737572+adamelalfy@users.noreply.github.com>
2024-11-22 15:50:12 -0500

    2 waves devrait être fonctionnel; reste à tester

--- Assets/ScenesSandBox/Adam/SpawnWavesFeature/Scripts/enemySpawner.cs
+++ Assets/ScenesSandBox/Adam/SpawnWavesFeature/Scripts/enemySpawner.cs
@@ -7,10 +7,16 @@ public class enemySpawner : MonoBehaviour
 
     [SerializeField] private GameObject enemy;
     [SerializeField] private GameObject spawnZone;
-    private float _interval = 0.5f;
+    private float _interval = 0.2f;
+    private int enemyCount = 0;
+    private bool enemiesSpawning = true;
+    public int enemiesKilled = 0;
+    public bool wave2Done = false;
 
     private void spawnEnemy()
     {
+        if (enemiesSpawning == true)
+        {   
         Transform spawnZoneTransform = spawnZone.transform;
         Vector3 spawnZoneScale = spawnZoneTransform.localScale;
         Vector3 spawnZonePosition = spawnZoneTransform.position;
@@ -21,13 +27,42 @@ public class enemySpawner : MonoBehaviour
             Random.Range(spawnZonePosition.z - spawnZoneScale.z / 2, spawnZonePosition.z + spawnZoneScale.z / 2)
         );
         Instantiate(enemy, randomPosition, Quaternion.identity);
+        enemyCount ++;
+        //Debug.Log(enemyCount);
+        Debug.Log("spawnEnemy is called");
+        }
+    }
+
+    private void stopSpawningEnemy(){
+        enemiesSpawning = false;
+    }
+
+    private void startWave2()
+    {
+        if (wave2Done == false)
+        {
+        _interval = 0.1f;
+        enemyCount = -20;
+        enemiesKilled = -20;
+        InvokeRepeating("spawnEnemy", 4f, _interval);
+        enemiesSpawning = true;
+        wave2Done = true;
+        Debug.Log("Wave 2 starts now");
+        }
     }
 
     private void Start() {
-        if(1 == 1)
-        {
         InvokeRepeating("spawnEnemy", 0f, _interval);
+    }
 
+    private void Update() {
+        if (enemyCount == 25)
+        {
+            stopSpawningEnemy();
+        }
+        if (enemiesKilled == 25)
+        {
+            startWave2();
         }
     }
 
