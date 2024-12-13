44ee2df629638d990f094e3d9c6feadb0801c80a
adamelalfy <143737572+adamelalfy@users.noreply.github.com>
2024-12-13 15:43:32 -0500

    2 waves fonctionnent

--- Assets/Scripts/enemySpawner.cs
+++ Assets/Scripts/enemySpawner.cs
@@ -5,39 +5,42 @@ using UnityEngine;
 public class enemySpawner : MonoBehaviour
 {
 
-    [SerializeField] private GameObject enemy;
-    [SerializeField] private GameObject spawnZone;
-    private float _interval = 0.2f;
-    private int enemyCount = 0;
-    private bool enemiesSpawning = true;
+    //[SerializeField] private GameObject enemy;
+    //[SerializeField] private GameObject spawnZone;
+    public float _interval = 0.2f;
+    public int enemyCount = 0;
+    public bool enemiesSpawning = true;
     public int enemiesKilled = 0;
     public bool wave2Done = false;
+    [SerializeField] private EnnemySpawner ennemySpawnerCastor;
+    [SerializeField] private EnnemySpawner ennemySpawnerCamion;
 
-    private void spawnEnemy()
+    public void spawnEnemy()
     {
         if (enemiesSpawning == true)
         {   
-        Transform spawnZoneTransform = spawnZone.transform;
-        Vector3 spawnZoneScale = spawnZoneTransform.localScale;
-        Vector3 spawnZonePosition = spawnZoneTransform.position;
+        // Transform spawnZoneTransform = spawnZone.transform;
+        // Vector3 spawnZoneScale = spawnZoneTransform.localScale;
+        // Vector3 spawnZonePosition = spawnZoneTransform.position;
 
-        Vector3 randomPosition = new Vector3(
-            Random.Range(spawnZonePosition.x - spawnZoneScale.x / 2, spawnZonePosition.x + spawnZoneScale.x / 2),
-            Random.Range(spawnZonePosition.y - spawnZoneScale.y / 2, spawnZonePosition.y + spawnZoneScale.y / 2),
-            Random.Range(spawnZonePosition.z - spawnZoneScale.z / 2, spawnZonePosition.z + spawnZoneScale.z / 2)
-        );
-        Instantiate(enemy, randomPosition, Quaternion.identity);
-        enemyCount ++;
-        //Debug.Log(enemyCount);
-        Debug.Log("spawnEnemy is called");
+        // Vector3 randomPosition = new Vector3(
+        //     Random.Range(spawnZonePosition.x - spawnZoneScale.x / 2, spawnZonePosition.x + spawnZoneScale.x / 2),
+        //     Random.Range(spawnZonePosition.y - spawnZoneScale.y / 2, spawnZonePosition.y + spawnZoneScale.y / 2),
+        //     Random.Range(spawnZonePosition.z - spawnZoneScale.z / 2, spawnZonePosition.z + spawnZoneScale.z / 2)
+        // );
+        // Instantiate(enemy, randomPosition, Quaternion.identity);
+        // enemyCount ++;
+        // //Debug.Log(enemyCount);
+        // Debug.Log("spawnEnemy is called");
+
         }
     }
 
-    private void stopSpawningEnemy(){
+    public void stopSpawningEnemy(){
         enemiesSpawning = false;
     }
 
-    private void startWave2()
+    public void startWave2()
     {
         if (wave2Done == false)
         {
@@ -51,7 +54,7 @@ public class enemySpawner : MonoBehaviour
         }
     }
 
-    private void Start() {
+    public void Start() {
         InvokeRepeating("spawnEnemy", 0f, _interval);
     }
 
